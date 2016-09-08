f2a3957495793df8a384c80607cdaf33c3bf6208
Kangb6187 <kangb6187@gmail.com>
2016-09-08 11:41:13 -0700

    Add files via upload

---
+++ APLesson_02/Lesson_02.java
@@ -0,0 +1,18 @@
+public class Lesson_02
+{
+	public static void main (String[]args)
+	{
+		String name = "WatMyName?";
+		System.out.println(name);
+		
+		System.out.println(8+5);
+		
+		short n1=6;
+		short n2=8;
+		System.out.println(n1*n2);
+		
+		String first="my name";
+		String second="is wat?";
+		System.out.println(first+second);
+	}
+}
