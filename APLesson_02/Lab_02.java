7b80935e6bbc17d6d854898eefe374bb8ae1c010
Kangb6187 <kangb6187@gmail.com>
2016-09-08 12:14:29 -0700

    Files Edited

---
+++ APLesson_02/Lab_02.java
@@ -0,0 +1,15 @@
+public class Lab_02
+{
+	public static void main (String[]args)
+	{
+		int n1=2;
+		int n2=4;
+		System.out.println("2 multiplied by 4 is "+ n1 * n2);
+		
+		int n3=5;
+		int n4=6;
+		System.out.println("5 multiplied by 6 is "+ n3*n4);
+		
+		String first=
+	}
+}
