411547f7f75c7f4f84cc89e7873620e120dcdb33
Kangb6187 <kangb6187@gmail.com>
2016-11-14 12:13:54 -0800

    Added Files

---
+++ APLesson_06/Lab_Table.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class Lab_Table
+{
+	public static void main(String[]args) 
+	{
+		Scanner kb = new Scanner(System.in);
+		System.out.println("Please enter an integer");
+		int integer = kb.nextInt(); 
+		System.out.println("What should the size of the table be?");
+		int tablesize = kb.nextInt(); 
+		System.out.println("_________________");
+		System.out.println(" |Equation: y = " + integer + "x|");
+		System.out.println("_________________");
+		
+		for (int i = 1; i < (1 + tablesize); i++) 
+		{
+			int y = integer * i;
+			format(i, y);
+		}
+		System.out.println("_________________");
+	}
+	public static void format(int x, int y) 
+	{
+		System.out.printf("|%3d |\t%5d\t|\n", x, y);
+	}
+}
